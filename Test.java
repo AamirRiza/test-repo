7a5a3e6ef1a3b7116567b78579638e609d046b8f
Aamir Mohammed <aamir_riza@Aamirs-MacBook-Air.local>
2024-01-14 20:22:52 +0530

    Java project
    Saving Java project

--- Test.java
+++ Test.java
@@ -1,7 +1,22 @@
 class Test {
 
+
+    public static void testMethod(){
+
+        int x = 3;
+        int y = 5;
+
+        int total  = x+y;
+
+        System.out.println(total);
+    }
+
     public static void main(String[] args) {
         System.out.println("This is testing out the GIT commands");
+
+        testMethod();
+        testMethod();
+        testMethod();
     }
 
 
